106bb8449f2053a48f619906f731c90f6b635145
Aditya <adishelar2005@gmail.com>
2026-08-26 11:20:19 +0530

    Reverce Array Elements

---
+++ Array/reverce_array.java
@@ -0,0 +1,8 @@
+public class reverce_array {
+    public static void main(String[] args) {
+        int a[]={23,45,67,88,55,10,56};
+        for(int i=a.length-1;i>=0;i--){
+            System.out.println(a[i]);
+        }
+    }
+}
