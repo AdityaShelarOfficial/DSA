ca350753131521a845ba6d7ef6411c5a2a6bb736
Aditya <adishelar2005@gmail.com>
2026-08-26 10:52:27 +0530

    print array elements

---
+++ Array/print_array_elements.java
@@ -0,0 +1,9 @@
+public class print_array_elements {
+    public static void main(String[] args) {
+        int a[]={234,664,35,7,47,9,75};
+        System.out.println("Array Elements:");
+        for(int i=0;i<a.length;i++){
+            System.out.print(a[i]+" ");
+        }
+    }
+}
