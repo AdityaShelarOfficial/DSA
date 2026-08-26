4800f4d9dda967d2f3534e1f843162b66c3814a4
Aditya <adishelar2005@gmail.com>
2026-08-26 11:45:36 +0530

    Search Specific element in array

---
+++ Array/length_array.java
@@ -0,0 +1,10 @@
+public class length_array {
+    public static void main(String[] args) {
+        int a[]={23,55,11,78,54,99};
+        int count=0;
+        for(int i=0;i<a.length;i++){
+            count++;
+        }
+        System.out.println("Count of all elements:"+count);
+    }
+}
