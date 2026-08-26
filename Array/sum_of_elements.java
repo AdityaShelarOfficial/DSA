580c5d993ab4f4266a4ae1524c44b977a20a04ee
Aditya <adishelar2005@gmail.com>
2026-08-26 09:44:07 +0530

    Sum of All Array Elements

---
+++ Array/sum_of_elements.java
@@ -0,0 +1,11 @@
+public class sum_of_elements {
+    public static void main(String[] args) {
+        int a[]={25,30,45};
+        int sum=0;
+        for(int i=0;i<a.length;i++){
+            sum=sum+a[i];
+        }
+        System.out.println("Sum 0f all Elements is:"+sum);
+
+    }
+}
