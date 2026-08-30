14493f669c1ca0fdbc36eee010a771e49fc6790e
Aditya <adishelar2005@gmail.com>
2026-08-30 16:37:07 +0530

    Maximum and minimum array element

---
+++ Array/max_min_array_element.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class max_min_array_element {
+    public static void main(String[] args) {
+        int a[]={12,34,56,78,98,43,22};
+        Arrays.sort(a);
+        System.out.println("Smallest element is:"+a[0]);
+        System.out.println("Largest element is the:"+a[a.length-1]);
+       
+    }
+}
