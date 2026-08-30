3361f28bb641202720221c69c1c302a9579e7f5e
Aditya <adishelar2005@gmail.com>
2026-08-30 16:22:02 +0530

    Sort array using arrays.sort function

---
+++ Array/sort_using_sort_function.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+public class sort_using_sort_function {
+    public static void main(String[] args) {
+        int a[]={12,34,56,87,54,76,10};
+        Arrays.sort(a);
+        for(int i=0;i<a.length;i++)
+        {
+            System.out.println(a[i]);
+        }
+    }
+}
