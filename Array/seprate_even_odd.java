b9f04240f78d329ae7ab94f08448ffa66d6361de
Aditya <adishelar2005@gmail.com>
2026-09-02 12:57:35 +0530

    Seprate the even and odd elements from an array

---
+++ Array/seprate_even_odd.java
@@ -0,0 +1,18 @@
+public class seprate_even_odd {
+    public static void main(String[] args) {
+        int a[]={12,45,67,88,98,12};
+        int index=0;
+        for(int i=0;i<a.length;i++){
+            if(a[i]%2==0){
+                int temp=a[i];
+                a[i]=a[index];
+                a[index]=temp;
+                index++;
+            }
+        }
+        for(int i=0;i<a.length;i++){
+            System.out.println(a[i]);
+        }
+        
+    }
+}
