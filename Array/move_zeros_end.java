a73ed774a36afc7534a2ec367fa66e307242055f
Aditya <adishelar2005@gmail.com>
2026-09-02 12:08:20 +0530

    Move all the zerors to end of array

---
+++ Array/move_zeros_end.java
@@ -0,0 +1,20 @@
+public class move_zeros_end {
+    public static void main(String[] args) {
+        int a[]={12,0,34,56,89,57,0,};
+        int index=0;
+        for(int i=0;i<a.length;i++){
+            if(a[i]!=0){
+                a[index]=a[i];
+                index++;
+            }
+            
+        }
+        while(index<a.length){
+                a[index]=0;
+                index++;
+            }
+        for(int i=0;i<a.length;i++){
+            System.out.println(a[i]);
+        }
+    }
+}
